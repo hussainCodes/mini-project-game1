c064d2e539df4b314e5f9348197317347306de86
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-21 22:28:16 +0300

    finish css and useState for player button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./assets/style.css"
+import deathStar from './assets/media/death-star.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+const [counter,setCounter] =useState(0);
+const [credit,setCredit] = useState(0);
+const add = () => {
+    setCounter(counter +1);
+    setCredit(credit +1);
 }
 
-export default App;
+
+
+
+  return (
+    <div className='main-div'>
+
+
+      <div className='header'>
+        <div>
+        <img src={deathStar} alt='death-star' className='pic'></img>
+        <h2> The death star</h2>
+        </div>
+       <div className='credit'>
+        <img className='coin-img' src='https://icones.pro/wp-content/uploads/2021/05/icone-base-donnees-jaune.png' alt='coins'/>
+        <h3>{credit}</h3>
+       </div>
+      </div>
+
+      <div className='player'>
+      <h1>{counter}</h1>
+      <h4>Planets Destroyed</h4>
+      <button className='player-button' onClick={add}>Destroy Planet</button>
+      <p>1 planet per click</p>
+      </div>
+
+      <div className='upgrade-list'>
+        <h3>available upgrades:</h3>
+        <div className='action-buttons'>
+
+          <button className='action-button'>Go</button>
+          <button className='action-button'>Go</button>
+          <button className='action-button'>Go</button> 
+          </div>
+          <p>Double Planets per click</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,14 @@
+const upgrades = [
+{
+     name:"drink dark-side blend coffee ",
+     descrption:"Double planets destroyed per click for 10 Galactic Credits",
+},
+{
+    name:"force choke an annoying admiral",
+    descrption:"Triples planets destroyed per click for 40 Galactic Credits ",
+},
+{
+    name:"Increase SuperLaser Power",
+    descrption:"Quadruples planets destroyed per click for 70 Galactic Credits ",
+}
+]
